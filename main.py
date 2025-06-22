b6131c63bad33e9c039f753b904eb2896d7a24bb
Pidone <pd@secfault.org>
2025-06-22 22:35:40 +0200

    Refactor to use the led_strip class

--- main.py
+++ main.py
@@ -1,98 +1,57 @@
 from machine import Pin
-from neopixel import NeoPixel
-from time import sleep_ms
 import uasyncio as asyncio
 import tinyweb
-import color
+from led_strip import color, LEDStrip
 
 # Datapin is on D7 = GPIO10
 DATA_PIN = Pin(10, Pin.OUT)
 NUMBER_OF_LEDS = 125
-LIGHT_WHITE = (50, 50, 50)
-OFF = (0, 0, 0)
 
-pixels = NeoPixel(DATA_PIN, NUMBER_OF_LEDS)
-
-for i in range(NUMBER_OF_LEDS):
-  pixels[i] = OFF
-
-pixels.write()
-
-
-modes = {'CUSTOM': 'custom' , 'ALL': 'all', 'OFF': 'off', 'RAINBOW': 'rainbow'}
-
-db = {
-  "current_state": {
-    "mode": modes['ALL'],
-    "data": OFF
-  }
-}
-
-def apply_state(state):
-  if state["mode"] == modes['ALL']:
-    for i in range(NUMBER_OF_LEDS):
-      pixels[i] = state['data']
-  elif state["mode"] == modes['CUSTOM']:
-    for i in range(NUMBER_OF_LEDS):
-      pixels[i] = state['data'][i]
-  elif state["mode"] == modes['OFF']:
-    for i in range(NUMBER_OF_LEDS):
-      pixels[i] = OFF
-  elif state["mode"] == modes['RAINBOW']:
-    step = 360 / NUMBER_OF_LEDS
-    offset = 0
-    brightness = 0.5
-    if "data" in state:
-      if "offset" in state["data"]:
-        offset = int(state["data"]["offset"])
-      if "brightness" in state["data"]:
-        brightness = float(state["data"]["brightness"])
-    
-    for i in range(NUMBER_OF_LEDS):
-      pixels[i] = color.hue_to_rgb(offset + step * i, brightness)
-
-
-  pixels.write()
-
-def get_current_pixel_state():
-  return [pixels[x] for x in range(NUMBER_OF_LEDS)]
+strip = LEDStrip(DATA_PIN, NUMBER_OF_LEDS)
+strip.off()
 
 class Config():
     def get(self, data):
         return {
-          "numberOfLeds": NUMBER_OF_LEDS,
-          "location": 'office',
-          "availableModes": list(modes.values())
+          "numberOfLeds": strip.number_of_leds,
+          "location": 'office'
         }
 
 class LedList():
-  def get(self, data):
-    return {"currentState": db["current_state"]}
+  def get(self, _data):
+    return {"leds": strip.get_pixels()}
 
   def put(self, data):
-    db["current_state"] = data
-    apply_state(db["current_state"])
-    return {"currentState": db["current_state"]}
+    pixels = data["leds"]
+    strip.set_pixels(pixels)
+    return {"leds": strip.get_pixels()}
 
 class Led():
   def not_exists(self):
-    return {'message': 'no such customer'}, 404
+    return {'message': 'no such led'}, 404
 
-  def get(self, data, index):
+  def get(self, _data, index):
     i = int(index)
-    return {"index": i, "color": pixels[i] }
+    if i >= strip.number_of_leds or i < 0:
+      return self.not_exists()
+
+    return {"index": i, "color": strip.get_pixel(i) }
 
   def put(self, data, index):
     i = int(index)
-    db["current_state"]['mode'] = modes['CUSTOM']
-    pixels[i] = data['color']
-    pixels.write()
-    db["current_state"]['data'] = get_current_pixel_state()
+    if i >= strip.number_of_leds or i < 0:
+      return self.not_exists()
 
-    return {"index": i, "color": pixels[i] }
+    strip.set_pixel(i, data["color"])
+    return {"index": i, "color": strip.get_pixel(i)}
 
-  def delete(self, data, index):
-    return {'message': 'successfully deleted'}
+  def delete(self, _data, index):
+    i = int(index)
+    if i >= strip.number_of_leds or i < 0:
+      return self.not_exists()
+
+    strip.set_pixel(i, (0, 0, 0))
+    return {"index": i, "color": strip.get_pixel(i)}
 
 app = tinyweb.webserver()
 app.add_resource(Config, '/config')
